8dc8c89555da4ad41709d639540af90fb51b0681
Kyle Gortych <99693659+KyleGortych@users.noreply.github.com>
2022-05-18 16:15:21 -0400

    Add files via upload

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -30,66 +30,72 @@ starship init fish | source
 
 # neofetch
 if status is-interactive
-neofetch --colors --colors 9 2 3 39 15 15 --backend iterm2 --source ~/Downloads/vim2.png --size 20%
-date +"-- %a %m-%d-%Y %I:%M%p -----------------------"
-echo -e ' '
-ps
+  # ~/CLI\ Support/sh\ Scripts/./info.sh
+ 
+  neofetch --colors --colors 9 2 3 39 15 15 --backend iterm2 --source ~/Downloads/vim2.png --size 20%
+  date +"-- %a %m-%d-%Y %I:%M%p --"
+  printf '\n'
+  ps
 end
 
 # aliases
 
 function aliases
-    column -t -s '|' < ~/Main\ Directory/CLI\ Support/Aliase\ Support/fish_aliases.txt | tr '*' ' '
+  column -t -s '|' < ~/CLI\ Support/Aliase\ Support/fish_aliases.txt | tr '*' ' '
+end
+
+function neo
+  neofetch --colors --colors 9 2 3 39 15 15 --backend iterm2 --source ~/Downloads/vim2.png --size 20%
 end
 
 function list-build-sys
-    cat ~/Main\ Directory/CLI\ Support/Aliase\ Support/list\ build\ sys.txt | tr '*' ' '
+  cat ~/CLI\ Support/Aliase\ Support/list\ build\ sys.txt | tr '*' ' '
 end
 
 function list-env
-    env | column -t -s '='
+  env | column -t -s '='
 end
 
 function brew-list-pkgs
-    column -t -s '|' < ~/Main\ Directory/CLI\ Support/Aliase\ Support/brew-pkgs.txt | tr '*' ' '
+  column -t -s '|' < ~/CLI\ Support/Aliase\ Support/brew-pkgs.txt | tr '*' ' '
 end
 
 function brew-search-active-pkgs
-    echo -e '\e[4mPackages no Depens\e[0m' ; brew leaves | column ; echo '' ; echo -e '\e[4mCasks\e[0m' ; brew list --cask
+  echo -e '\e[4mPackages no Depens\e[0m' ; brew leaves | column ; echo '' ; echo -e '\e[4mCasks\e[0m' ; brew list --cask
 end
 
 function pip3-list-pkgs
-    pip3 list --not-required
+  pip3 list --not-required
 end
 
 function npm-env
-    echo -e pkgs\n----- && npm list -g --depth=0 && echo -e env\n---- && npm config list
+  echo -e pkgs\n----- && npm list -g --depth=0 && echo -e env\n---- && npm config list
 end
 
 function open-app
-    open -a "$argv" && exit
+  open -a "$argv" && exit
 end
 
 function file-manager
-    cd $(xplr --print-pwd-as-result)
+  cd $(xplr --print-pwd-as-result)
 end
 
 function apl-run-script
-    apl --noSV --noColor --noCIN -q -f $argv
+  apl --noSV --noColor --noCIN -q -f $argv
 end
 
 function verilog-compile
-    iverilog -o $argv
+  iverilog -o $argv
 end
 
 function verilog-run
-    vvp $argv
+  vvp $argv
 end
 
 function doom
-    ~/.emacs.d/bin/doom $argv
+  ~/.emacs.d/bin/doom $argv
 end
 
 # function kotlin-run-script
-#     kotlinc $argv -include-runtime -d $argv && java -jar $argv
+#   kotlinc $argv -include-runtime -d $argv && java -jar $argv
 # end
