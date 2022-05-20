1890cad921fbc665e3c5ec8333a78849dd3ed4dd
Kyle Gortych <99693659+KyleGortych@users.noreply.github.com>
2022-05-20 15:42:05 -0400

    Add files via upload

--- CLI Support/sh Scripts/info.sh
+++ CLI Support/sh Scripts/info.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # note: add xterm escape sequences to neo.txt | font size and color
-column -t -s '|' < ~/CLI\ Support/Aliase\ Support/neo.txt 
+column -t -s '|' < ~/CLI\ Support/sh\ scripts/info.sh
 date +"-- %a %m-%d-%Y %I:%M%p --"
 printf '\n'
 ps
