a69440ccfec1a3cbb61efe13ef9067e6985c54e7
Kyle Gortych <99693659+KyleGortych@users.noreply.github.com>
2022-05-22 18:53:28 -0400

    Delete CLI Support directory

--- CLI Support/sh Scripts/info.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# note: add xterm escape sequences to neo.txt | font size and color
-column -t -s '|' < ~/CLI\ Support/sh\ scripts/info.sh
-date +"-- %a %m-%d-%Y %I:%M%p --"
-printf '\n'
-ps
