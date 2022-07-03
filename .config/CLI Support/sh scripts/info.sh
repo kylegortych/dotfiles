1dac7023d19086bbce33b1ae06933051d7ca3aae
Kyle Gortych <kylegortych@Kyles-MacBook-Pro.local>
2022-07-03 19:31:00 -0400

    Major change 3

--- .config/CLI Support/sh scripts/info.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# note: add xterm escape sequences to neo.txt | font size and color
-column -t -s '|' < ~/.config/CLI\ Support/aliase\ \&\ script\ support/neo.txt 
-date +"-- %a %m-%d-%Y %I:%M%p --"
-printf '\n'
-ps
